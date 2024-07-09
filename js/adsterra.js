1adeee6e5f6ad66a8822e1b1e8f75e1a0cdf6426
camelbakone <157678022+camelbakone@users.noreply.github.com>
2024-07-09 16:54:50 -0500

    Add files via upload

---
+++ js/adsterra.js
@@ -0,0 +1,8 @@
+// Ads terra Historia
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = '//cessationhamster.com/0b/81/cb/0b81cb8dff1c33fbc54ecfdd2f86e592.js';
+
+// Agregar el elemento <script> al final del body
+document.body.appendChild(script);
+
